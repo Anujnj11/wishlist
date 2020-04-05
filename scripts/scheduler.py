a85cf8d53f705f474cadf5e6d49ad7d2138e9ea0
Anuj Gupta <anuj@tavaga.com>
2020-04-05 17:09:55 +0530

    chnages for api

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -1,11 +1,18 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import userWishScrape
 
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('interval', minutes=5)
 def timed_job():
     print('This job is run every three minutes.')
+
+
+@sched.scheduled_job('interval', minutes=60)
+def scrape_wish():
+    print('Getting wishlist price.')
+    userWishScrape.get_active_wish()
 
 
 sched.start()
